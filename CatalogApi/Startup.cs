d9ba00e82b1a74a28764eef1ef47511371ad5c9f
litchi <d0509327@mail.fcu.edu.tw>
2021-01-24 01:27:51 +0800

    can connect SQL Server and insert info by code

---
+++ CatalogApi/Startup.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CatalogApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string server = Configuration["DatabaseServer"];
+            string database = Configuration["DatabaseName"];
+            string user = Configuration["DatabaseUser"];
+            string password = Configuration["DatabasePassword"];
+            string connectionString = string.Format("Server={0};Database={1};User={2};Password={3};",
+                server, database, user, password);
+
+            services.AddControllers();
+            services.AddDbContext<CatalogContext>(options => options.UseSqlServer(connectionString));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-RPGShop
+# RPGShop
+
+## 建立商品服務的Api
+### 01/24
+**Can connect with SQL Server and insert info by code**  
+1. Can't login by sa and new account
+2. Error with *services.AddDbContext()*
+
+
+---
+##### 參考網址: https://ithelp.ithome.com.tw/users/20128651/ironman/3738
